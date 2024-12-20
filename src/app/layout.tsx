05f623b6193ea2a59cbaa6977f5e44f0da5efc6f
Roman <ixrock@gmail.com>
2024-12-20 17:45:49 +0200

    feat: added custom fonts to webpage from `next/font/google` package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type React from "react";
+import type { Metadata } from "next";
+import { Open_Sans, Roboto_Flex } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const robotoFlex = Roboto_Flex({
+  variable: "--font-roboto",
+  subsets: ["latin-ext", "cyrillic-ext"],
 });
 
 export const metadata: Metadata = {
@@ -19,15 +20,10 @@ export const metadata: Metadata = {
   description: "translator app for modern browsers (free extension)",
 };
 
-export default function RootLayout(
-  {
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
-    <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <body className={`${openSans.variable} ${robotoFlex.variable}`}>
     {children}
     </body>
     </html>
