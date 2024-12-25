6abbf025e95c4443ae737617e9753b4fce867abc
Roman <ixrock@gmail.com>
2024-12-25 14:11:55 +0200

    chore: fine-tuning page contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,26 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <section className={styles.installIcons}>
-        <a href="https://chromewebstore.google.com/detail/xtranslate/gfgpkepllngchpmcippidfhmbhlljhoo" target="_blank">
-          <img src="/chrome.svg" alt="Install from Chrome Extensions store"/>
+        <a
+          href="https://chromewebstore.google.com/detail/xtranslate/gfgpkepllngchpmcippidfhmbhlljhoo"
+          title="Install from Chrome Extensions store"
+          target="_blank">
+          <img src="/chrome.svg" alt="Chrome Extensions Store"/>
         </a>
-        <a href="https://microsoftedge.microsoft.com/addons/detail/xtranslate/cinfaflgbaachkaamaeglolofeahelkd" target="_blank">
-          <img src="/edge.svg" alt="Install from Microsoft Edge Extensions store"/>
+        <a
+          href="https://microsoftedge.microsoft.com/addons/detail/xtranslate/cinfaflgbaachkaamaeglolofeahelkd"
+          title="Install from Microsoft Edge Extensions Store"
+          target="_blank">
+          <img src="/edge.svg" alt="MS Edge Store"/>
         </a>
-        <a href="https://github.com/ixrock/XTranslate" target="_blank">
+        <a href="https://github.com/ixrock/XTranslate" title="Github" target="_blank">
           <img src="/github.svg" alt="Install from source code"/>
         </a>
       </section>
 
       <header className={styles.header}>
         <img src="/xtranslate-icon.png" className={styles.logo} alt="XTranslate Logo"/>
-        <h1>XTranslate - your AI translator agent (browser translation extension)</h1>
+        <h1>XTranslate - your AI translator agent (browser extension)</h1>
       </header>
 
       <p>It will help you understand and get <b>real-time text translation</b> of foreign languages or get <b>full-page text translation</b> in your browser (chromium-based)</p>
@@ -30,20 +36,20 @@ export default function Home() {
 
       <PhotoPreviews images={[
         { src: "/sshots/website_popup.jpg", alt: "Get translation in web page context, default popup theme" },
-        { src: "/sshots/website_translation_results.png", alt: "Popup custom theme with action icons (LTR: Text-to-speech, Save to favorites, Copy translation, Next translation)" },
         { src: "/sshots/website_select_text.png", alt: "Different ways to get translation from selected page text, e.g. clicking by near-by [X]-icon" },
+        { src: "/sshots/website_translation_results.png", alt: "Popup custom theme with action icons (LTR: Text-to-speech, Save to favorites, Copy translation, Next translation)" },
         { src: "/sshots/app_settings.png", alt: "Configure extension app settings" },
-        { src: "/sshots/app_popup_customization.png", alt: "Configure theme and customize translation popup" },
-        { src: "/sshots/app_translate_tab.png", alt: "Input text and get translation from main app window" },
         { src: "/sshots/app_history_tab.png", alt: "History of translations with import/export in JSON-format" },
         { src: "/sshots/pdf_translation.png", alt: "Translate foreign texts in PDF files" },
+        { src: "/sshots/app_popup_customization.png", alt: "Configure theme and customize translation popup" },
+        { src: "/sshots/app_translate_tab.png", alt: "Input text and get translation from main app window" },
       ]}
       />
 
       <h3>Install extension:</h3>
       <hr/>
       <ul>
-        <li><a href="https://chrome.google.com/webstore/detail/xtranslate/gfgpkepllngchpmcippidfhmbhlljhoo" target="_blank">Chrome's Web Store</a></li>
+        <li><a href="https://chrome.google.com/webstore/detail/xtranslate/gfgpkepllngchpmcippidfhmbhlljhoo" target="_blank">Chrome Extensions Web Store</a></li>
         <li><a href="https://microsoftedge.microsoft.com/addons/detail/cinfaflgbaachkaamaeglolofeahelkd" target="_blank">Microsoft Edge Add-ons</a></li>
         <li><a href="https://addons.mozilla.org/en-GB/firefox/addon/xtranslate-chrome/" target="_blank">Firefox addons</a> <em>(old version with limited support)</em></li>
         <li><a href="https://github.com/ixrock/XTranslate" target="_blank">Github</a> <em>(from source code)</em></li>
@@ -51,19 +57,21 @@ export default function Home() {
 
       <h3>Features:</h3>
       <hr/>
-      <p>Many ways to get translation of text from the page:</p>
+      <p>Many ways to <em>get text translation</em> from web page or PDF files:</p>
       <ul>
-        <li>double-click on the word</li>
-      </ul>
-      <p>select text and get immediate translation after one of:</p>
-      <ol>
+        <li>Double-click on the word</li>
         <li>Press hotkey defined in extension settings (<em>Alt+Shift+X</em> by default)</li>
         <li>Click on the translate icon appeared near selected text</li>
-        <li>Click on selected text (turned off by default)</li>
-        <li>Just release mouse button (turned off by default)</li>
+        <li>Click on selected text <em>(turned off by default)</em></li>
+        <li>Just release mouse button <em>(turned off by default)</em></li>
         <li>Write text in input fields, mouse over it and press hotkey</li>
-        <li>Press context menu on the page and select item to get translation in place or translate whole page in new tab with specific translation vendor</li>
-      </ol>
+        <li>Get translation of selected text from via <em>XTranslate</em> context menu item</li>
+        <li>
+          You can translate whole pages with <a href="https://translate.google.com/" target="_blank">Google</a>
+          <span> or </span>
+          <a href="https://translate.yandex.com" target="_blank">Yandex</a> services for free <em>(via context menu)</em>
+        </li>
+      </ul>
 
       <p>
         You can get even some information from images by putting mouse cursor over the image element and
@@ -71,14 +79,10 @@ export default function Home() {
         This trick actually works with any element under mouse cursor. Just put mouse over item and press hotkey!
       </p>
 
-      <h3>Translate texts in PDF-files (<small>this will replace default PDF-viewer with pdf.js (mozilla's custom viewer)</small>)</h3>
-
-      <p>
-        <em>In order to work with local files (HTML or PDF) allow access for the extension:</em>
-      </p>
+      <h3>In order to work with local files (HTML or PDF) allow access for the extension:</h3>
       <ol>
         <li>open extensions URL system page <code>chrome://extensions</code>, find <b>XTranslate</b>™ and click <em>[Details]</em> button</li>
-        <li>enable checkbox <b>Allow access to file URLs</b></li>
+        <li>enable checkbox <em>Allow access to file URLs</em></li>
       </ol>
 
       <h3>Free features of <em>XTranslate</em> extension are includes:</h3>
@@ -103,8 +107,8 @@ export default function Home() {
       <h3>Available translation vendors:</h3>
       <hr/>
       <ul>
-        <li><a href="http://translate.google.com/" target="_blank">Google</a> (free)</li>
-        <li><a href="http://translate.yandex.com/" target="_blank">Yandex</a> (free)</li>
+        <li><a href="https://translate.google.com/" target="_blank">Google</a> (free)</li>
+        <li><a href="https://translate.yandex.com/" target="_blank">Yandex</a> (free)</li>
         <li><a href="https://www.bing.com/translator" target="_blank">Bing</a> (free)</li>
         <li><a href="https://www.deepl.com/translator" target="_blank">DeepL</a> (free: API key limited to 500,000 chars/month)</li>
         <li><a href="https://platform.openai.com" target="_blank">OpenAI (ChatGPT authors)</a> (paid: register and top-up balance)</li>
@@ -123,20 +127,21 @@ export default function Home() {
       <hr/>
       <ul>
         <li>
-          OpenAI api key handled via extension-scoped chrome.storage.local apis and used only within "Authorization" header to sign OpenAI API requests
+          OpenAI api key handled via extension-scoped <code>chrome.storage.local</code> apis and used only within <code>Authorization</code> header to sign OpenAI API requests
           (which is not exposed or tracked, even if <a href="https://developer.chrome.com/docs/extensions/reference/api/webRequest">webRequest</a> API enabled in some other malicious extension)
         </li>
         <li>
-          OpenAI requests goes through official OpenAI NPM-package and running only within background service-worker which helps with XSS-kind of attacks.
+          OpenAI requests goes through official <a href="https://github.com/openai/openai-node" target="_blank">OpenAI NPM-package</a> and running only within background service-worker which helps
+          with XSS-kind of attacks.
         </li>
         <li>
-          Don't enter or share your OpenAI key anywhere else except extension's settings page (options page)
+          Don't enter or share your OpenAI key anywhere else except extension's settings page <em>(options page)</em>
         </li>
       </ul>
       <hr/>
       <br/>
       <footer>
-        Made with ♥ and open-source projects. Donate for free-version app support via <a href="https://www.paypal.me/romanesca" target="_blank">PayPal</a>
+        Made with ♥ and open-source projects. Donate for free app version support via <a href="https://www.paypal.me/romanesca" target="_blank">PayPal</a>
       </footer>
     </div>
   );
